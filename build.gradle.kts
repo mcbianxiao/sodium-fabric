9e83c27059cc8f4704eba5ff4f81ab6986f5d901
BianXiao <137746769+mcbianxiao@users.noreply.github.com>
2024-09-17 20:43:00 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -129,7 +129,7 @@ fun createVersionString(): String {
         builder.append(Constants.MOD_VERSION)
     } else {
         builder.append(Constants.MOD_VERSION.substringBefore('-'))
-        builder.append("-snapshot")
+        builder.append("-snapshot-byBianXiaoforHoly")
     }
 
     builder.append("+mc").append(Constants.MINECRAFT_VERSION)
